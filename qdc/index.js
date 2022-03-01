035a073353efe75fcfac550395c9f99fa2ba6a4f
Saikat Das <saikatdas8258377@gmail.com>
2022-03-01 23:10:30 +0530

    3rd installation

--- qdc/index.js
+++ qdc/index.js
@@ -1,4 +1,88 @@
 $(document).ready(function () {
+  var i = 0;
+  var data = [
+    [
+      "01",
+      "Feb 2022 2:22:14 PM",
+      "114",
+      "Triloke Nath Kushwaha",
+      "	Delhi",
+      "7503848724",
+      "04 Feb 2022",
+      "3",
+      "0",
+      "300",
+      "0",
+      "354",
+      "0",
+      "354",
+    ],
+    [
+      "02",
+      "Feb 2022 2:22:14 PM",
+      "114",
+      "Triloke Nath Kushwaha",
+      "	Delhi",
+      "7503848724",
+      "04 Feb 2022",
+      "3",
+      "0",
+      "300",
+      "0",
+      "354",
+      "0",
+      "354",
+    ],
+    [
+      "03",
+      "Feb 2022 2:22:14 PM",
+      "114",
+      "Triloke Nath Kushwaha",
+      "	Delhi",
+      "7503848724",
+      "04 Feb 2022",
+      "3",
+      "0",
+      "300",
+      "0",
+      "354",
+      "0",
+      "354",
+    ],
+    [
+      "04",
+      "Feb 2022 2:22:14 PM",
+      "114",
+      "Triloke Nath Kushwaha",
+      "	Delhi",
+      "7503848724",
+      "04 Feb 2022",
+      "3",
+      "0",
+      "300",
+      "0",
+      "354",
+      "0",
+      "354",
+    ],
+    [
+      "05",
+      "Feb 2022 2:22:14 PM",
+      "114",
+      "Triloke Nath Kushwaha",
+      "	Delhi",
+      "7503848724",
+      "04 Feb 2022",
+      "3",
+      "0",
+      "300",
+      "0",
+      "354",
+      "0",
+      "354",
+    ],
+  ];
+
   $("#toggle").hide();
   $("#filter").click(function () {
     $("#toggle").toggle(0, "linear");
@@ -11,17 +95,25 @@ $(document).ready(function () {
   });
 
   $("#add").click(() => {
-    var html =
-      "<tr id='1'><td>Order Date/Time</td><td>Order No.</td><td>Address</td><td>Phone</td><td>Due Date</td><td>Pcs.</td><td>Weight</td><td>Gross Amount</td><td>Disconnect</td><td>Net Ammount</td><td>Paid</td><td>Balance</td></tr>";
+    var html = "<tr id=" + i + ">";
+    for (var j = 0; j < 14; j++) {
+      html += "<td>" + data[i][j] + "</td>";
+    }
     $("#table_body").append(html);
+    i++;
+
+    if (i === 5) {
+      i = 0;
+    }
   });
 
-  $("#customer_name").keyup(function () {
+  $("#customer_name,#user").keyup(function () {
     this.value = this.value.replace(/[^a-z\^A-Z\ ]/g, ""); //Aplahabets only
     //this.value = this.value.replace(/[^0-9\.]/g, ""); //Numbers only
   });
   var names = ["Saikat", "Sabuj", "Asit", "Iti"];
-  $("#customer_name").autocomplete({ source: names }, () => {
+  $("#customer_name,#user").autocomplete({ source: names }, () => {
     console.log("working");
   });
+  
 });
